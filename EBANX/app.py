61e672ad8fddc058786096e38a875246c7874252
schwabandre <69318708+schwabandre@users.noreply.github.com>
2024-07-07 20:15:17 -0300

    Update app.py

--- EBANX/app.py
+++ EBANX/app.py
@@ -1,31 +1,48 @@
 #Ponto de entrada da aplicação Flask e usará os serviços definidos em 'services.py'
 
 from flask import Flask, request, jsonify
-from services import reset_system, get_balance, create_event
+from services import reset_system, get_balance, deposit, withdraw, transfer
 
 app = Flask(__name__)
-
 
 @app.route('/reset', methods=['POST'])
 def reset():
     reset_system()
-    return jsonify({"message": "System reset successful"}), 200
+    return 'OK', 200
 
 @app.route('/balance', methods=['GET'])
 def balance():
     account_id = request.args.get('account_id')
     balance = get_balance(account_id)
     if balance is None:
-        return jsonify({"message": "Account not found"}), 404
-    return jsonify({"balance": balance}), 200
+        return '0', 404
+    return str(balance), 200
 
 @app.route('/event', methods=['POST'])
 def event():
-    event_data = request.get_json()
-    result = create_event(event_data)
-    if 'message' in result:
-        return jsonify({"message": result['message']}), 404
-    return jsonify(result), 201
+    data = request.json
+    event_type = data.get("type")
+
+    if event_type == "deposit":
+        destination = data.get("destination")
+        amount = data.get("amount")
+        result, status_code = deposit(destination, amount)
+        return jsonify(result), status_code
+
+    elif event_type == "withdraw":
+        origin = data.get("origin")
+        amount = data.get("amount")
+        result, status_code = withdraw(origin, amount)
+        return jsonify(result), status_code
+
+    elif event_type == "transfer":
+        origin = data.get("origin")
+        destination = data.get("destination")
+        amount = data.get("amount")
+        result, status_code = transfer(origin, destination, amount)
+        return jsonify(result), status_code
+
+    return '0', 400
 
 if __name__ == '__main__':
     app.run(debug=True)
