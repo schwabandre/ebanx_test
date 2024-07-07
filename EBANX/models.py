ff44fa3bf72a43c3131bcf6fdc5af331484f5936
schwabandre <69318708+schwabandre@users.noreply.github.com>
2024-07-07 20:15:31 -0300

    Update models.py

--- EBANX/models.py
+++ EBANX/models.py
@@ -1,23 +1,4 @@
-#Em memória "Banco de dados"
 class Account:
-    def __init__(self, account_id, balance = 0):
-        self.account_id = account_id
+    def __init__(self, id, balance):
+        self.id = id
         self.balance = balance
-
-    def deposit(self, amount):
-        self.balance += amount
-
-    def withdraw(self, amount):
-        if self.balance >= amount:
-            self.balance -= amount
-            return True
-        return False
-    
-    def transfer(self, amount, destination_account):
-        if self.withdraw(amount):
-            destination_account.deposit(amount)
-            return True
-        return False
-
-
-accounts = {}
